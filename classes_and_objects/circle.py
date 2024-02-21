ef57965f52319f66232925c5b65ba51669789ddc
Viviancat666 <vivian9069@gmail.com>
2024-02-21 18:56:39 +0200

    Create circle.py

---
+++ classes_and_objects/circle.py
@@ -0,0 +1,16 @@
+class Circle:
+    pi = 3.14
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+    def get_area(self):
+        return Circle.pi * self.radius ** 2
+
+    def get_circumference(self):
+        return Circle.pi * 2 * self.radius
+
+
