71778e89781ab9b0edd60dd79960526955e2784d
Viviancat666 <vivian9069@gmail.com>
2024-02-24 00:54:38 +0200

    Create __init__.py

---
+++ inheritance_exercise/zoo/project/__init__.py
@@ -0,0 +1,8 @@
+# from animal import Animal
+# from bear import Bear
+# from gorilla import Gorilla
+# from lizard import Lizard
+# from mammal import Mammal
+# from reptile import Reptile
+# from snake import Snake
+#
