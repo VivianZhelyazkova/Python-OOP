eaf5fc5bcb0d57225cdb32113ed0d927a51a8608
Viviancat666 <vivian9069@gmail.com>
2024-02-23 02:26:45 +0200

    Update __init__.py

--- classes_and_objects_exercise/Library/project/__init__.py
+++ classes_and_objects_exercise/Library/project/__init__.py
@@ -1,18 +1,12 @@
-from library import Library
-from user import User
-from registration import Registration
-
-
-
-user = User(12, 'Peter')
-library = Library()
-registration = Registration()
-registration.add_user(user, library)
-library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
-                                                'The Prisoner of Azkaban',
-                                                'The Goblet of Fire',
-                                                'The Order of the Phoenix',
-                                                'The Half-Blood Prince',
-                                                'The Deathly Hallows']})
-library.get_book('J.K.Rowling', 'The Deathly Hallows', 10, user)
-print(user)
+# from project.library import library
+# from project.user import User
+# from registration import Registration
+#
+#
+# user = User(12, 'Peter')
+# library = library()
+# registration = Registration()
+# registration.add_user(user, library)
+#
+# registration.remove_user(user, library)
+# print(library.user_records)
