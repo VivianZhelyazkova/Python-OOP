9998ed5e0b3bbfaf3030d11eee279e5a87f6493d
Viviancat666 <vivian9069@gmail.com>
2024-02-24 02:31:42 +0200

    Homework

---
+++ inheritance_exercise/players_and_monsters/project/__init__.py
@@ -0,0 +1,22 @@
+# from blade_knight import BladeKnight
+# from dark_knight import DarkKnight
+# from dark_wizard import DarkWizard
+# from elf import Elf
+# from hero import Hero
+# from knight import Knight
+# from muse_elf import MuseElf
+# from soul_master import SoulMaster
+# from wizard import Wizard
+#
+#
+# hero = Hero("H", 4)
+# print(hero.username)
+# print(hero.level)
+# print(str(hero))
+# elf = Elf("E", 4)
+# print(str(elf))
+# print(elf.__class__.__bases__[0].__name__)
+# print(elf.username)
+# print(elf.level)
+
+
---
+++ inheritance_exercise/players_and_monsters/project/dark_knight.py
@@ -0,0 +1,6 @@
+from knight import Knight
+
+
+class DarkKnight(Knight):
+    def __init__(self, username: str, level: int):
+        super().__init__(username, level)
