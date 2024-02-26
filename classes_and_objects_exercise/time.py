d9a641d6821b23fcd30a46d1ecd7a1f18e3bf8f5
Viviancat666 <vivian9069@gmail.com>
2024-02-26 19:25:01 +0200

    Update time.py

--- classes_and_objects_exercise/time.py
+++ classes_and_objects_exercise/time.py
@@ -14,7 +14,6 @@ class Time:
         self.seconds = seconds
 
     def get_time(self):
-        # return f"{self.hours}:{self.minutes}:{self.seconds}"
         return "{:02d}:{:02d}:{:02d}".format(self.hours, self.minutes, self.seconds)
 
     def next_second(self):
@@ -22,10 +21,10 @@ class Time:
         if self.seconds > Time.max_seconds:
             self.seconds = 00
             self.minutes += 1
-        if self.minutes > Time.max_minutes:
-            self.minutes = 00
-            self.hours += 1
-        if self.hours > Time.max_hours:
-            self.hours = 00
+            if self.minutes > Time.max_minutes:
+                self.minutes = 00
+                self.hours += 1
+                if self.hours > Time.max_hours:
+                    self.hours = 00
         return self.get_time()
 
