33324689397156fc72abc6b7df3bd4ec6c04ebe0
Viviancat666 <vivian9069@gmail.com>
2024-02-26 03:30:28 +0200

    Create person.py

---
+++ encapsulation/person.py
@@ -0,0 +1,38 @@
+class Person:
+    def __init__(self, name: str, age: int):
+        self.__validate_name(name)
+        self.__validate_age(age)
+        self.__name = name
+        self.__age = age
+
+    def __validate_name(self, name: str):
+        if len(name) < 3:
+            raise ValueError("name must be at least 3 characters")
+
+    def __validate_age(self, age: int):
+        if age < 18:
+            raise ValueError("Age must be over 18")
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @name.setter
+    def name(self, name: str):
+        self.__validate_name(name)
+        self.__name = name
+
+    @age.setter
+    def age(self, age: int):
+        self.__validate_age(age)
+        self.__age = age
+
+
+person = Person("egf", 54)
+person.name = "et"
+print(person.name)
+print(person.age)
