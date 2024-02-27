9bd19e06fad6a6fad9aaf8878d5723a057f6063d
Viviancat666 <vivian9069@gmail.com>
2024-02-28 01:39:46 +0200

    Update integer.py

--- class_and_static_methods/integer.py
+++ class_and_static_methods/integer.py
@@ -7,10 +7,31 @@ class Integer:
 
     @classmethod
     def from_float(cls, float_value):
-        if float_value is not float:
+        if type(float_value) != float:
             return "value is not a float"
-        return cls(floor(float_value))
+        return cls(int(float_value))
 
     @classmethod
-    def from_roman(cls,value):
-        
+    def from_roman(cls, value):
+        roman_numerals = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400,
+                          'D': 500, 'CM': 900, 'M': 1000}
+        result = 0
+        i = 0
+        while i < len(value):
+            if i + 1 < len(value) and value[i:i + 2] in roman_numerals:
+                result += roman_numerals[value[i:i + 2]]
+                i += 2
+            else:
+                result += roman_numerals[value[i]]
+                i += 1
+        return cls(result)
+
+    @classmethod
+    def from_string(cls, value):
+
+        if type(value) == str:
+            return cls(int(value))
+        else:
+            return "wrong type"
+
+
