6d459a02c2b67365f85c6de4a3a956883b694d43
Viviancat666 <vivian9069@gmail.com>
2024-02-29 02:11:01 +0200

    Create dvd.py

---
+++ class_and_static_methods_exercise/movie_world/project/dvd.py
@@ -0,0 +1,20 @@
+import calendar
+
+
+class DVD:
+    def __init__(self, name: str, _id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = _id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, _id: int, name: str, date: str, age_restriction: int):
+        day, month, year = date.split(".")
+        return cls(name, _id, int(year), calendar.month_name[int(month)], age_restriction)
+
+    def __repr__(self):
+        return f'{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction {self.age_restriction}. Status: {"rented" if self.is_rented else "not rented"}'
+
