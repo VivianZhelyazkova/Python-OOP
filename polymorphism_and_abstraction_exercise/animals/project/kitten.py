be82d2c98b002ba520a716810109273b36b42873
Viviancat666 <vivian9069@gmail.com>
2024-03-01 21:59:54 +0200

    Create kitten.py

---
+++ polymorphism_and_abstraction_exercise/animals/project/kitten.py
@@ -0,0 +1,10 @@
+from project.cat import Cat
+
+
+class Kitten(Cat):
+    def __init__(self, name: str, age: int, gender="Female"):
+        super().__init__(name, age, gender)
+
+    def make_sound(self):
+        return "Meow"
+
