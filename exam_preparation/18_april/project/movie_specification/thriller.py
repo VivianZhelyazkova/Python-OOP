7837678746b7e7ddf32c5466d346d1eb8fe5ab17
Viviancat666 <vivian9069@gmail.com>
2024-03-14 03:20:52 +0200

    Create thriller.py

---
+++ exam_preparation/18_april/project/movie_specification/thriller.py
@@ -0,0 +1,16 @@
+from project.movie_specification.movie import Movie
+
+
+class Thriller(Movie):
+    def __init__(self, title: str, owner: object, year: int, age_restriction=16):
+        super().__init__(title, year, owner, age_restriction)
+
+    @Movie.age_restriction.setter
+    def age_restriction(self, value):
+        if value < 16:
+            raise ValueError("Thriller movies must be restricted for audience under 16 years!")
+        self._age_restriction = value
+
+    def details(self):
+        return (f"Thriller - Title:{self.title}, Year:{self.year}, Age restriction:{self.age_restriction}"
+                f", Likes:{self.likes}, Owned by:{self.owner.username}")
