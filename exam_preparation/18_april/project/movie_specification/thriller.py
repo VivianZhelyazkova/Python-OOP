3a2ac4a5aaaeba4b404b397ee9cbaaf32fa82364
Viviancat666 <vivian9069@gmail.com>
2024-03-14 21:35:32 +0200

    Update thriller.py

--- exam_preparation/18_april/project/movie_specification/thriller.py
+++ exam_preparation/18_april/project/movie_specification/thriller.py
@@ -2,15 +2,19 @@ from project.movie_specification.movie import Movie
 
 
 class Thriller(Movie):
-    def __init__(self, title: str, owner: object, year: int, age_restriction=16):
-        super().__init__(title, year, owner, age_restriction)
+    def __init__(self, title: str, year: int, owner: object, age_restriction=16):
+        super().__init__(title, year, owner)
+        self.age_restriction = age_restriction
 
-    @Movie.age_restriction.setter
+    @property
+    def age_restriction(self):
+        return self.__age_restriction
+
+    @age_restriction.setter
     def age_restriction(self, value):
         if value < 16:
             raise ValueError("Thriller movies must be restricted for audience under 16 years!")
-        self._age_restriction = value
+        self.__age_restriction = value
 
     def details(self):
-        return (f"Thriller - Title:{self.title}, Year:{self.year}, Age restriction:{self.age_restriction}"
-                f", Likes:{self.likes}, Owned by:{self.owner.username}")
+        return (f"Thriller - Title:{self.title}, Year:{self.year}, Age restriction:{self.age_restriction}, Likes:{self.likes}, Owned by:{self.owner.username}")
