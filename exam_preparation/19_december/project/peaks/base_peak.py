325192cae4f106611dff9ddc5251a4e520bd36b9
Viviancat666 <vivian9069@gmail.com>
2024-03-18 04:40:21 +0200

    Create base_peak.py

---
+++ exam_preparation/19_december/project/peaks/base_peak.py
@@ -0,0 +1,36 @@
+import abc
+
+
+class BasePeak(abc.ABC):
+    def __init__(self, name: str, elevation: int):
+        self.name = name
+        self.elevation = elevation
+        self.difficulty_level = self.calculate_difficulty_level()
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) < 2:
+            raise ValueError("Peak name cannot be less than 2 symbols!")
+        self.__name = value
+
+    @property
+    def elevation(self):
+        return self.__elevation
+
+    @elevation.setter
+    def elevation(self, value: int):
+        if value < 1500:
+            raise ValueError("Peak elevation cannot be below 1500m.")
+        self.__elevation = value
+
+    @abc.abstractmethod
+    def get_recommended_gear(self):
+        pass
+
+    @abc.abstractmethod
+    def calculate_difficulty_level(self):
+        pass
