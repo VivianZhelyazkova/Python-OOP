1be5aabde5c418ab3aac921ba36152132f5bc8b1
Viviancat666 <vivian9069@gmail.com>
2024-03-19 04:03:27 +0200

    Create predatory_fish.py

---
+++ exam_preparation/09_december/project/fish/predatory_fish.py
@@ -0,0 +1,11 @@
+from project.fish.base_fish import BaseFish
+
+
+class PredatoryFish(BaseFish):
+    TIME_TO_CATCH = 90
+
+    def __init__(self, name: str, points: float):
+        super().__init__(name, points, PredatoryFish.TIME_TO_CATCH)
+
+    def fish_details(self):
+        return f"{self.__class__.__name__}: {self.name} [Points: {self.points}, Time to Catch: {PredatoryFish.TIME_TO_CATCH} seconds]"
