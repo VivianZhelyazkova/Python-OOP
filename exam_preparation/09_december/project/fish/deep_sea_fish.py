3c1d77ee7471814d510e34ab86526dc253a8e8a7
Viviancat666 <vivian9069@gmail.com>
2024-03-19 04:03:29 +0200

    Create deep_sea_fish.py

---
+++ exam_preparation/09_december/project/fish/deep_sea_fish.py
@@ -0,0 +1,11 @@
+from project.fish.base_fish import BaseFish
+
+
+class DeepSeaFish(BaseFish):
+    TIME_TO_CATCH = 180
+
+    def __init__(self, name: str, points: float):
+        super().__init__(name, points, DeepSeaFish.TIME_TO_CATCH)
+
+    def fish_details(self):
+        return f"{self.__class__.__name__}: {self.name} [Points: {self.points}, Time to Catch: {DeepSeaFish.TIME_TO_CATCH} seconds]"
