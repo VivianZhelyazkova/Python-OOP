8d3dc70dc6d52e2b7891ac7b17dd91536946f8ec
Viviancat666 <vivian9069@gmail.com>
2024-03-20 01:56:35 +0200

    Create main.py

---
+++ exam_preparation/09_december/project/main.py
@@ -0,0 +1,46 @@
+from project.divers.free_diver import FreeDiver
+from project.divers.scuba_diver import ScubaDiver
+from project.fish.deep_sea_fish import DeepSeaFish
+from project.fish.predatory_fish import PredatoryFish
+from project.nautical_catch_challenge_app import NauticalCatchChallengeApp
+
+nautical_catch_challenge = NauticalCatchChallengeApp()
+
+# Dive into competition
+print(nautical_catch_challenge.dive_into_competition("ScubaDiver", "MaxineHarper"))
+print(nautical_catch_challenge.dive_into_competition("FreeDiver", "JamalCarter"))
+print(nautical_catch_challenge.dive_into_competition("SkyDiver", "FionaBennett"))
+print(nautical_catch_challenge.dive_into_competition("FreeDiver", "OscarWallace"))
+print(nautical_catch_challenge.dive_into_competition("ScubaDiver", "LilaMoreno"))
+print(nautical_catch_challenge.dive_into_competition("FreeDiver", "LilaMoreno"))
+
+# Swim into competition
+print(nautical_catch_challenge.swim_into_competition("ReefFish", "AzureDamselfish", 8.7))
+print(nautical_catch_challenge.swim_into_competition("DeepSeaFish", "BluestripeSnapper", 6.3))
+print(nautical_catch_challenge.swim_into_competition("PredatoryFish", "YellowtailSurgeonfish", 5.0))
+print(nautical_catch_challenge.swim_into_competition("PredatoryFish", "Barracuda", 9.2))
+print(nautical_catch_challenge.swim_into_competition("PredatoryFish", "Coryphaena", 9.7))
+print(nautical_catch_challenge.swim_into_competition("PredatoryFish", "Bluefish", 4.4))
+print(nautical_catch_challenge.swim_into_competition("DeepSeaFish", "SwordFish", 10.0))
+print(nautical_catch_challenge.swim_into_competition("DeepSeaFish", "Mahi-Mahi", 9.1))
+print(nautical_catch_challenge.swim_into_competition("DeepSeaFish", "Tuna", 8.5))
+print(nautical_catch_challenge.swim_into_competition("AquariumFish", "SilverArowana", 3.3))
+print(nautical_catch_challenge.swim_into_competition("DeepSeaFish", "Barracuda", 8.6))
+
+# Conduct fishing competitions
+
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", False))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", False))
+
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
+print(nautical_catch_challenge.chase_fish("MaxineHarper", "Coryphaena", True))
