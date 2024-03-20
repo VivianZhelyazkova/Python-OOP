c29183d70f9d7fb87e5656cabb36238147a158a1
Viviancat666 <vivian9069@gmail.com>
2024-03-20 20:54:50 +0200

    Update even_numbers.py

--- decorators/even_numbers.py
+++ decorators/even_numbers.py
@@ -1,6 +1,6 @@
 def even_numbers(function):
     def wrapper(numbers):
-        result = [num for num in numbers if num % 2 == 0]
+        result = function([num for num in numbers if num % 2 == 0])
         return result
 
     return wrapper
