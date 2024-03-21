1cb7507de43d8cba6524c0efcc636cc216248132
Viviancat666 <vivian9069@gmail.com>
2024-03-21 21:36:34 +0200

    Update tournament.py

--- exam_preparation/15_august/project/tournament.py
+++ exam_preparation/15_august/project/tournament.py
@@ -40,3 +40,54 @@ class Tournament:
         team = eval(team_type)(team_name, country, advantage)
         self.teams.append(team)
         return f"{team_type} was successfully added."
+
+    def sell_equipment(self, equipment_type: str, team_name: str):
+        equipment_list = [obj for obj in self.equipment if obj.__class__.__name__ == equipment_type]
+        equipment = equipment_list[-1]
+        team = next((obj for obj in self.teams if obj.name == team_name), None)
+        if team.budget < equipment.price:
+            raise Exception("Budget is not enough!")
+        self.equipment.remove(equipment)
+        team.equipment.append(equipment)
+        team.budget -= equipment.price
+        return f"Successfully sold {equipment_type} to {team_name}."
+
+    def remove_team(self, team_name: str):
+        team = next((obj for obj in self.teams if obj.name == team_name), None)
+        if not team:
+            raise Exception("No such team!")
+        if team.wins:
+            raise Exception(f"The team has {team.wins} wins! Removal is impossible!")
+        self.teams.remove(team)
+        return f"Successfully removed {team_name}."
+
+    def increase_equipment_price(self, equipment_type: str):
+        count = 0
+        for item in self.equipment:
+            if item.__class__.__name__ == equipment_type:
+                item.increase_price()
+                count += 1
+        return f"Successfully changed {count}pcs of equipment."
+
+    def play(self, team_name1: str, team_name2: str):
+        first = next((obj for obj in self.teams if obj.name == team_name1), None)
+        second = next((obj for obj in self.teams if obj.name == team_name2), None)
+        if first.__class__.__name__ != second.__class__.__name__:
+            raise Exception("Game cannot start! Team types mismatch!")
+        first_score = sum(x.protection for x in first.equipment) + first.advantage
+        second_score = sum(x.protection for x in second.equipment) + second.advantage
+        if first_score > second_score:
+            first.win()
+            return f"The winner is {team_name1}."
+        elif first_score < second_score:
+            second.win()
+            return f"The winner is {team_name2}."
+        else:
+            return "No winner in this game."
+
+    def get_statistics(self):
+        sorted_teams = sorted(self.teams, key=lambda x: -x.wins)
+        result = f"Tournament: {self.name}\nNumber of Teams: {len(self.teams)}\nTeams:"
+        for team in sorted_teams:
+            result += "\n" + team.get_statistics()
+        return result
