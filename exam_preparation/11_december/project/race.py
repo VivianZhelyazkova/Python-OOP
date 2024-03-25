98cad182d786c2990b113f5bd6002cffe83b1f2d
Viviancat666 <vivian9069@gmail.com>
2024-03-25 21:45:35 +0200

    Create race.py

---
+++ exam_preparation/11_december/project/race.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from project.driver import Driver
+
+
+class Race:
+    def __init__(self, name: str):
+        self.name = name
+        self.drivers: List[Driver] = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            raise ValueError("Name cannot be an empty string!")
+        self.__name = value
+
