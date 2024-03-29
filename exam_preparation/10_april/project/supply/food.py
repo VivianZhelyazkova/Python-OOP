679e46b3113e10d93f863ee0e476554109b8afe7
Viviancat666 <vivian9069@gmail.com>
2024-03-29 02:52:18 +0200

    Update food.py

--- exam_preparation/10_april/project/supply/food.py
+++ exam_preparation/10_april/project/supply/food.py
@@ -2,10 +2,9 @@ from project.supply.supply import Supply
 
 
 class Food(Supply):
-    ENERGY = 25
 
-    def __init__(self, name: str):
-        super().__init__(name, Food.ENERGY)
+    def __init__(self, name: str, energy=25):
+        super().__init__(name, energy)
 
     def details(self):
         return f"{self.__class__.__name__}: {self.name}, {self.energy}"
