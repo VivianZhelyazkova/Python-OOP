038b4bb98de70e3157590eb7084ccb302a569c8b
Viviancat666 <vivian9069@gmail.com>
2024-04-04 20:45:50 +0300

    Create guitarist.py

---
+++ exam_preparation/19_december_2/project/band_members/guitarist.py
@@ -0,0 +1,16 @@
+from project.band_members.musician import Musician
+
+
+class Guitarist(Musician):
+    VALID_SKILLS = ["play metal",
+                    "play rock",
+                    "play jazz"
+                    ]
+
+    def learn_new_skill(self, new_skill: str):
+        if new_skill not in Guitarist.VALID_SKILLS:
+            raise ValueError(f"{new_skill} is not a needed skill!")
+        if new_skill in self.skills:
+            raise Exception(f"{new_skill} is already learned!")
+        self.skills.append(new_skill)
+        return f"{self.name} learned to {new_skill}."
