aa74a3b30e34cf684baf8f9dec182a3c7cba0b70
Viviancat666 <vivian9069@gmail.com>
2024-04-04 20:45:54 +0300

    Create band.py

---
+++ exam_preparation/19_december_2/project/band.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from project.band_members.drummer import Drummer
+from project.band_members.guitarist import Guitarist
+from project.band_members.musician import Musician
+from project.band_members.singer import Singer
+
+
+class Band:
+    def __init__(self, name: str):
+        self.name = name
+        self.members: List[Musician] = []
+        self.musicians = {
+            Singer: 0,
+            Drummer: 0,
+            Guitarist: 0,
+        }
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not value.strip():
+            raise ValueError("Band name should contain at least one character!")
+        self.__name = value
+
+    def __str__(self):
+        return f"{self.name} with {len(self.members)} members."
